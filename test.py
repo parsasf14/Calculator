3e4a52a2280a1083b80d325fd0c1ef2678783e3a
Parsa <82938129+parsasf14@users.noreply.github.com>
2026-06-23 00:50:23 -0700

    Add files via upload

--- test.py
+++ test.py
@@ -1,18 +1,37 @@
 while True:
+    print("Choose A Number In : 1-6")
+    print("for Tutorial Choose : 5")
+    print("Exit! : 6")
     Gogol = int(input("Type:"))
-    P = int(input("Input:"))
-    S = int(input("Input2:"))
-    if Gogol not in range (1,5):
+    if Gogol not in range (1,7):
       print("Invalid Number!")
       continue
+    if Gogol == 5:
+      print("Tutorial:")
+      print("1 : +")
+      print("2 : *")
+      print("3 : /")
+      print("4 : -")
+      print("5 : Tutorial")
+      print("6 : Exit!")  
+      continue
+    elif Gogol == 6:
+      print("(((Exit!)))")
+      break
+    num1 = int(input("Input:"))
+    num2 = int(input("Input2:"))
     if Gogol == 1:
 
-     print(S+P)
+     print(num1+num2)
     elif Gogol == 2:
-     print(S * P)
+     print(num1 * num2)
 
     elif Gogol == 3:
-      print (round(S / P))
+      if num2 == 0:
+        print("This is Impossible!")
+        continue
+      else:
+        print (round(num1 / num2,2))
 
     elif Gogol == 4:
-      print(S - P)  
+      print(num1 - num2)  
