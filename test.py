87f9c41b0dbfe46d8bc5f2175bcb31923508ee49
Parsa <82938129+parsasf14@users.noreply.github.com>
2026-06-22 22:54:35 -0700

    Add files via upload

---
+++ test.py
@@ -0,0 +1,11 @@
+while True:
+    Gogol = input("Type:")
+    P = int(input("Input"))
+    S = int(input("Input2"))
+    if Gogol == "1":
+
+     print(S+P)
+    elif Gogol == "2":
+     print(S * P)
+
+    elif Gogol == "3"
